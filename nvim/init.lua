25fb149ba2e67512ec02a6ac1ae039db232e9b5a
“claudio-code” <suporte2424@outlook.com>
2024-03-04 17:13:24 -0300

    changed to lazyvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,21 +1,25 @@
-require "core"
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
 
-local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
+-- local lspconfig = require("lspconfig")
 
-if custom_init_path then
-  dofile(custom_init_path)
-end
-
-require("core.utils").load_mappings()
-
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-
--- bootstrap lazy.nvim!
-if not vim.loop.fs_stat(lazypath) then
-  require("core.bootstrap").gen_chadrc_template()
-  require("core.bootstrap").lazy(lazypath)
-end
-
-dofile(vim.g.base46_cache .. "defaults")
-vim.opt.rtp:prepend(lazypath)
-require "plugins"
+-- lspconfig.jdtls.setup({
+--   settings = {
+--     java = {
+--       configuration = {
+--         runtimes = {
+--           {
+--             name = "JavaSE-21",
+--             path = "${HOME}/.sdkman/candidates/java/21.0.1-graalce",
+--             -- default = true,
+--           },
+--           {
+--             name = "JavaSE-17",
+--             path = "${HOME}/.sdkman/candidates/java/17.0.7-oracle",
+--             -- default = true,
+--           },
+--         }
+--       }
+--     }
+--   }
+-- })
