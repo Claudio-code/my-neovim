b179147145617bbb41b9333afce9bb130caf0361
Claudio Silva Junior <42524939+Claudio-code@users.noreply.github.com>
2024-04-21 21:19:08 -0300

    Update colorscheme.lua

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,20 +1,20 @@
 return {
-  {
-    "Mofiqul/adwaita.nvim",
-    lazy = false,
-    priority = 1000,
-  },
+  -- {
+  --   "Mofiqul/adwaita.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  -- },
 
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "adwaita",
-    },
-    config = function()
-        -- vim.g.adwaita_darker = true             -- for darker version
-        -- vim.g.adwaita_disable_cursorline = true -- to disable cursorline
-        -- vim.g.adwaita_transparent = true        -- makes the background transparent
-        vim.cmd('colorscheme adwaita')
-    end
-  },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "adwaita",
+  --   },
+  --   config = function()
+  --       -- vim.g.adwaita_darker = true             -- for darker version
+  --       -- vim.g.adwaita_disable_cursorline = true -- to disable cursorline
+  --       -- vim.g.adwaita_transparent = true        -- makes the background transparent
+  --       vim.cmd('colorscheme adwaita')
+  --   end
+  -- },
 }
